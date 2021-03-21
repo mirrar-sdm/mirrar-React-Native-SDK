8984405694e829cddcedfadb9c5e3d95558e178e
shivammaindola <shivammaindola07@gmail.com>
2021-03-21 23:25:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import Reactfrom 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class App extends Component<{}> {
+
+  render() {
+    return (
+      <View>
+        <Text style={{fontSize:20}}>☆MirrarSDK example☆</Text>
+      </View>
+    );
+  }
+}
+
